417a59cb7ae5c958f2d9e2a7f4e06327629f7373
Auriel James Fernandez <aurieljames11@gmail.com>
2023-04-18 23:47:53 +0800

    last edit

---
+++ src/components/Content.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Content(){
+    return (
+        <div className="Content">
+            <h1>Welcome to my Website</h1>
+            <h3>Come on ! and explore more about anime </h3>
+            <form className="Form">
+                <input type='text'/>
+                <button type="submit"> Search </button>
+            </form>
+        </div>
+    )
+}
+
+export default Content
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer(){
+    return (
+        <div  className="Footer">
+            <p>© 2023 Fernandez, All Rights Reserved</p>
+        </div>
+    )
+}
+
+
+export default Footer
