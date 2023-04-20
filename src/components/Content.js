5cbe64801be5e5062167509c97b54681440697f1
Auriel James Fernandez <aurieljames11@gmail.com>
2023-04-20 22:09:40 +0800

    applied changes

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,26 +2,39 @@ import React, {useEffect, useState} from "react";
 
 
 function Content(props){
-   
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+    
+    const [resourceType, setResourceType] = useState('posts')
+    const [items, setItems] = useState([])
 
-    function sizeGetter(){
-        setWindowWidth(window.innerWidth)
-    }
+
 
     useEffect(()=>{
-        window.addEventListener('resize', sizeGetter)
-    }, [])
+        fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then(response => response.json())
+      .then(json => setItems(json))
+    }, [resourceType])
+ 
+    let data = items.map((item)=>{
+        return <pre>ID: {JSON.stringify(item.id)} Name: {JSON.stringify(item.name)}</pre>
+    })
     return (
         <div className="Content">
             <h1>Welcome to my Website</h1>
+            <h2>{resourceType}</h2>
+           
+           
             <h3>Come on ! and explore more about anime </h3>
+
+       
             <div className="Form">
                 <input type='text'/>
                 <button> Search </button>
-            </div>
 
-            <h3>{windowWidth}</h3>
+            </div>
+            <button onClick={()=>{setResourceType('users')}}>USERS</button>
+            <button onClick={()=>{setResourceType('posts')}}>POST</button>
+            <button onClick={()=>{setResourceType('comments')}}>COMMENTS</button>
+            <h3>{data}</h3>
         </div>
     )
 }
